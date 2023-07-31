c33993ef2ee8a10ae64c6a0a40b1d5f52dd53d2f
edsuu <111332935+edsuuu@users.noreply.github.com>
2023-07-31 10:21:56 -0300

    att

--- src/assets/js/app.js
+++ src/assets/js/app.js
@@ -4,13 +4,13 @@ function createClock(segundos) {
         hour12: false,
         timeZone: 'GMT',
     });
-
 }
 
 const relogio = document.querySelector('#relogio');
 const play = document.querySelector('#play');
 const pause = document.querySelector('#pause');
 const clear = document.querySelector('#clear');
+const texto = document.querySelector('#texto');
 let segundos = 0;
 let timer;
 
@@ -24,14 +24,18 @@ function iniciarRelogio() {
 play.addEventListener('click', function (event) {
     relogio.classList.remove('pausado');
     relogio.classList.remove('zerado');
+
     clearInterval(timer);
     iniciarRelogio();
 });
+
 pause.addEventListener('click', function (event) {
     relogio.classList.remove('zerado');
     relogio.classList.add('pausado');
+    
     clearInterval(timer);
 });
+
 clear.addEventListener('click', function (event) {
     relogio.classList.remove('pausado');
     relogio.classList.add('zerado');
@@ -40,3 +44,15 @@ clear.addEventListener('click', function (event) {
     segundos = 0;
 });
 
+function typeWrite(elemento){
+    const textoArray = elemento.innerHTML.split('');
+    elemento.innerHTML = '';
+    textoArray.forEach(function(letra, i){   
+    setTimeout(function(){
+        elemento.innerHTML += letra;
+    }, 400 * i)
+  });
+}
+
+const titulo = document.querySelector('.titulo-principal');
+typeWrite(titulo);
