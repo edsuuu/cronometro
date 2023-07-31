4f7c012f34eabe6a4bbb3f50224bedc45008d4c6
edsuu <111332935+edsuuu@users.noreply.github.com>
2023-07-30 21:28:55 -0300

    Primeiro Commit

---
+++ src/assets/js/app.js
@@ -0,0 +1,39 @@
+function createClock(segundos) {
+    const data = new Date(segundos * 1000);
+    return data.toLocaleTimeString('pt-BR', {
+        hour12: false,
+        timeZone: 'GMT',
+    });
+
+}
+
+const relogio = document.querySelector('#relogio');
+const play = document.querySelector('#play');
+const pause = document.querySelector('#pause');
+const clear = document.querySelector('#clear');
+let segundos = 0;
+let timer;
+
+function iniciarRelogio() {
+    timer = setInterval(function () {
+        segundos++;
+        relogio.innerHTML = createClock(segundos);
+    }, 1000);
+}
+
+play.addEventListener('click', function (event) {
+    relogio.classList.remove('pausado');
+    clearInterval(timer);
+    iniciarRelogio();
+});
+pause.addEventListener('click', function (event) {
+    relogio.classList.add('pausado');
+    clearInterval(timer);
+});
+clear.addEventListener('click', function (event) {
+    relogio.classList.remove('pausado');
+    clearInterval(timer);
+    relogio.innerHTML = '00:00:00';
+    segundos = 0;
+});
+
